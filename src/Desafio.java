c2867fdbddfc6e1dcd2bd91f84ac0e1381a8db89
Endreews <150464197+Endreews@users.noreply.github.com>
2024-03-08 14:41:41 -0300

    Update Desafio.java

--- src/Desafio.java
+++ src/Desafio.java
@@ -1,2 +1,14 @@
-package PACKAGE_NAME;public class Desafio {
+public class Desafio {
+    public static void main(String[] args) {
+        int INDICE = 13;
+        int SOMA = 0;
+        int K = 0;
+
+        while (K < INDICE) {
+            K++;
+            SOMA = SOMA + K;
+        }
+        System.out.println(SOMA);
+        //Resultado: 91
+    }
 }
